3ce876a04ae43c96dedd8d77af5dc8311b9d4245
Elina Zaitseva <elinochkat932@gmail.com>
2024-06-01 15:16:58 +0300

    make homework

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+ document.addEventListener('DOMContentLoaded', () => {
+
+        const categoriesList = document.querySelectorAll('#categories .item')
+    
+        console.log(`Number of categories: ${categoriesList.length}`)
+    
+        categoriesList.forEach(category => {
+            const categoryName = category.querySelector('h2')
+            const elementsCount = category.querySelectorAll('ul li')
+    
+            console.log(`Category: ${categoryName.textContent}`)
+            console.log(`Elements: ${elementsCount.length}`)
+        })
+        
+    })
